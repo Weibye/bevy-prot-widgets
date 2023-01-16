76f7fa315a3167da82560263cdefda19ee516ed2
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2023-01-16 19:37:28 +0100

    Add RadioGroup Widget & Radio Widget example (#19)
    * Working radio-group
    
    * Adding more radio-widget examples

--- examples/radio-widget.rs
+++ examples/radio-widget.rs
@@ -2,13 +2,23 @@
 
 use bevy::{
     prelude::{
-        default, App, BuildChildren, Camera2dBundle, Color, Commands, EntityBlueprint, NodeBundle,
-        Res,
+        default, App, BuildChildren, Camera2dBundle, Color, Commands, Entity, EntityBlueprint,
+        NodeBundle, Res,
     },
     ui::{AlignItems, FlexDirection, JustifyContent, Size, Style, Val},
     DefaultPlugins,
 };
-use bevy_prot_widgets::{theme::WidgetTheme, widget::radio::RadioBlueprint, WidgetPlugin};
+use bevy_prot_widgets::{
+    theme::WidgetTheme,
+    widget::{
+        label::LabelWidgetBlueprint,
+        radio::{RadioBlueprint, RadioGroup},
+    },
+    WidgetPlugin,
+};
+use bevy_ui::UiRect;
+
+const COLOR_CONTENT_EXAMPLE: Color = Color::rgb(0.055, 0.12, 0.19);
 
 fn main() {
     App::new()
@@ -24,25 +34,184 @@ fn setup_camera(mut cmd: Commands) {
     cmd.spawn(Camera2dBundle::default());
 }
 
+// TODO: Setup a scroll-bar widget
 fn setup_page(mut cmd: Commands, theme: Res<WidgetTheme>) {
+    let mut radio_group_01: Vec<Entity> = vec![];
+    let mut radio_group_02: Vec<Entity> = vec![];
+    // root node
     cmd.spawn(NodeBundle {
         style: Style {
             size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
             justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            flex_direction: FlexDirection::Row,
             ..default()
         },
         background_color: theme.background_color.into(),
         ..default()
     })
     .with_children(|root| {
-        for _ in 0..12 {
-            RadioBlueprint {
-                checked: false,
-                theme: theme.icon.clone(),
+        // Content container
+        root.spawn(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Auto, Val::Percent(100.0)),
+                padding: UiRect::all(Val::Px(30.0)),
+                flex_direction: FlexDirection::Column,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|content| {
+            LabelWidgetBlueprint {
+                text: "Radio Group".into(),
+                theme: theme.h1.clone(),
             }
-            .build(&mut root.spawn_empty());
-        }
+            .build(&mut content.spawn_empty());
+
+            LabelWidgetBlueprint {
+                text: "`RadioGroup` is a helpful wrapper used to group `Radio` widgets together."
+                    .into(),
+                theme: theme.p.clone(),
+            }
+            .build(&mut content.spawn_empty());
+
+            // Example Showcase Contaner
+            content
+                .spawn(NodeBundle {
+                    style: Style {
+                        margin: UiRect::vertical(Val::Px(10.0)),
+                        padding: UiRect::vertical(Val::Px(10.0)),
+                        flex_direction: FlexDirection::Column,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    background_color: COLOR_CONTENT_EXAMPLE.into(),
+                    ..default()
+                })
+                .with_children(|example_showcase| {
+                    LabelWidgetBlueprint {
+                        text: "Cardinal Direction".into(),
+                        theme: theme.h2.clone(),
+                    }
+                    .build(&mut example_showcase.spawn_empty());
+
+                    // Group
+                    example_showcase
+                        .spawn((NodeBundle {
+                            style: Style {
+                                flex_direction: FlexDirection::Column,
+                                ..default()
+                            },
+                            ..default()
+                        },))
+                        .with_children(|group| {
+                            for direction in ["North", "South", "East", "West"] {
+                                group
+                                    .spawn(NodeBundle {
+                                        style: Style {
+                                            flex_direction: FlexDirection::Row,
+                                            ..default()
+                                        },
+                                        ..default()
+                                    })
+                                    .with_children(|entry| {
+                                        let mut entity = entry.spawn_empty();
+                                        radio_group_01.push(entity.id());
+
+                                        RadioBlueprint {
+                                            checked: false,
+                                            theme: theme.icon.clone(),
+                                        }
+                                        .build(&mut entity);
+
+                                        LabelWidgetBlueprint {
+                                            text: direction.into(),
+                                            theme: theme.h3.clone(),
+                                        }
+                                        .build(&mut entry.spawn_empty());
+                                    });
+                            }
+                        });
+                });
+
+            LabelWidgetBlueprint {
+                text: "Direction".into(),
+                theme: theme.h1.clone(),
+            }
+            .build(&mut content.spawn_empty());
+
+            LabelWidgetBlueprint {
+                text: "Can also do horizontal direction.".into(),
+                theme: theme.p.clone(),
+            }
+            .build(&mut content.spawn_empty());
+
+            // Example Showcase Contaner
+            content
+                .spawn(NodeBundle {
+                    style: Style {
+                        margin: UiRect::vertical(Val::Px(10.0)),
+                        padding: UiRect::vertical(Val::Px(10.0)),
+                        flex_direction: FlexDirection::Column,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    background_color: COLOR_CONTENT_EXAMPLE.into(),
+                    ..default()
+                })
+                .with_children(|example_showcase| {
+                    LabelWidgetBlueprint {
+                        text: "Cardinal Direction".into(),
+                        theme: theme.h2.clone(),
+                    }
+                    .build(&mut example_showcase.spawn_empty());
+
+                    // Group
+                    example_showcase
+                        .spawn((NodeBundle {
+                            style: Style {
+                                flex_direction: FlexDirection::Row,
+                                ..default()
+                            },
+                            ..default()
+                        },))
+                        .with_children(|group| {
+                            for direction in ["North", "South", "East", "West"] {
+                                group
+                                    .spawn(NodeBundle {
+                                        style: Style {
+                                            flex_direction: FlexDirection::Row,
+                                            ..default()
+                                        },
+                                        ..default()
+                                    })
+                                    .with_children(|entry| {
+                                        let mut entity = entry.spawn_empty();
+                                        radio_group_02.push(entity.id());
+
+                                        RadioBlueprint {
+                                            checked: false,
+                                            theme: theme.icon.clone(),
+                                        }
+                                        .build(&mut entity);
+
+                                        LabelWidgetBlueprint {
+                                            text: direction.into(),
+                                            theme: theme.h3.clone(),
+                                        }
+                                        .build(&mut entry.spawn_empty());
+                                    });
+                            }
+                        });
+                });
+        });
+    });
+
+    cmd.spawn(RadioGroup {
+        entities: radio_group_01.into(),
+    });
+
+    cmd.spawn(RadioGroup {
+        entities: radio_group_02.into(),
     });
 }
--- src/widget/radio.rs
+++ src/widget/radio.rs
@@ -1,7 +1,7 @@
 use bevy_app::{App, Plugin};
 
 use bevy_ecs::{
-    prelude::{Bundle, Component, EntityBlueprint, IntoSystemDescriptor},
+    prelude::{Bundle, Component, Entity, EntityBlueprint, IntoSystemDescriptor},
     query::Changed,
     system::{EntityCommands, Query},
 };
@@ -23,7 +23,12 @@ impl Plugin for RadioPlugin {
 
 /// Marker component for a CheckBoxWidget
 #[derive(Component, Debug, Clone, Default)]
-pub struct RadioWidget(pub bool);
+pub struct Radio(pub bool);
+
+#[derive(Component, Debug)]
+pub struct RadioGroup {
+    pub entities: Vec<Entity>,
+}
 
 pub struct RadioBlueprint {
     pub checked: bool,
@@ -44,7 +49,7 @@ impl EntityBlueprint for RadioBlueprint {
         }
         .build(entity);
 
-        entity.insert((RadioWidget(self.checked), Button, Interaction::default()));
+        entity.insert((Radio(self.checked), Button, Interaction::default()));
     }
 }
 
@@ -59,7 +64,7 @@ pub struct RadioBundle {
     /// Interaction state of the widget
     pub interaction: Interaction,
     /// Marker to make it a "CheckboxWidget"
-    pub radio: RadioWidget,
+    pub radio: Radio,
     // /// State of the widget
     // pub toggle: ToggleState,
     // /// The different icons for the widget
@@ -67,16 +72,47 @@ pub struct RadioBundle {
 }
 
 pub(crate) fn update_radio_interaction(
-    mut q: Query<(&mut RadioWidget, &Interaction), Changed<Interaction>>,
+    mut radio: Query<(Entity, &mut Radio, &Interaction)>, // TODO: See if this can be improved with change trackers Changed<Interaction>>,
+    radio_group: Query<&RadioGroup>,
 ) {
-    for (mut radio, interaction) in &mut q {
+    let mut group_changes = vec![];
+
+    for (entity, _, interaction) in &mut radio {
         if matches!(interaction, Interaction::Clicked) {
-            radio.0 = !radio.0;
+            // If this was clicked,
+            // check if it belongs in a group
+            let mut result = None;
+
+            for group in &radio_group {
+                if group.entities.iter().any(|e| *e == entity) {
+                    // radio is member of this group
+                    result = Some(group);
+                    break;
+                }
+            }
+
+            if let Some(result_group) = result {
+                group_changes.push((entity, result_group));
+            } else {
+                // This is not part of a group, so we can just flip it here.
+                // radio_widget.0 = !radio_widget.0;
+            }
+        }
+    }
+
+    for (clicked_entity, group) in group_changes {
+        for member in &group.entities {
+            if let Ok((_, mut other_radio, _)) = radio.get_mut(*member) {
+                other_radio.0 = false;
+            }
+        }
+        if let Ok((_, mut radio_widget, _)) = radio.get_mut(clicked_entity) {
+            radio_widget.0 = true;
         }
     }
 }
 
-pub fn update_radio_icon(mut q: Query<(&RadioWidget, &mut IconWidget), Changed<RadioWidget>>) {
+pub fn update_radio_icon(mut q: Query<(&Radio, &mut IconWidget), Changed<Radio>>) {
     for (radio, mut icon) in &mut q {
         icon.0 = if radio.0 {
             Icon::RadioButtonChecked
